cd7be70a1210ce5f73db68030ef56239ec08c1df
Rick <rick.ensink@ns.nl>
2024-12-03 17:14:08 +0100

    woop woop

--- Day 3/Day3.py
+++ Day 3/Day3.py
@@ -1,38 +1,54 @@
 import re
 
-# variables
-file = open('Day 3\Input files\Input.txt', 'r')
-# file = open('Day 3\Input files\TestInput.txt', 'r')
-# file = open('Day 3\Input files\TestInputPart2.txt', 'r')
-lineList = file.readlines()
-memory = ''
-part1answer = 0
-part2answer = 0
+## variables ##
+patternPart1 = r"(mul)\((\d{1,3}),(\d{1,3})\)"
+patternPart2 = r"don't\(\)(.*?)do\(\)"
+filePaths = {
+    '1': 'Day 3\Input files\Input.txt',
+    '2': 'Day 3\Input files\TestInput.txt',
+    '3': 'Day 3\Input files\TestInputPart2.txt'
+}
 
-# Process file
-for line in lineList:
-    memory += line
-memory += 'do()'
+## Methods ##
+def parseFile(filepath):
+    parsedFile = ""
+    with open(filepath, 'r') as file:
+        for line in file:
+            parsedFile += line
+    # This is needed for part 2
+    parsedFile += 'do()'
+    return parsedFile
+
+def part1(input):
+    part1answer = 0
+    uncorruptMemory = re.findall(patternPart1, input)
+    for multiplication in uncorruptMemory:
+        part1answer += (int(multiplication[1]) * int(multiplication[2]))
+    return part1answer
+
+def part2(input, part1answer):
+    part2answer = 0
+    doNotMemoryList = re.findall(patternPart2, input, re.DOTALL)
+    for donotMemory in doNotMemoryList:
+        multiplicationList = re.findall(patternPart1, donotMemory)
+        for multiplication in multiplicationList:
+            part2answer += (int(multiplication[1]) * int(multiplication[2]))
+    return part1answer - part2answer
+
+## Main execution ##
+
+# Prompt user for input choice and parse file
+print("""
+Select input file to use:
+    1. Main Input
+    2. Test Input
+    3. Test Input Part 2
+""")
+choice = input("Enter choice (1/2/3): ")
+input = parseFile(filePaths[choice])
 
 # Part 1
-uncorruptMemory = []
-patternPart1 = r"(mul)\((\d{1,3}),(\d{1,3})\)"
-uncorruptMemory = re.findall(patternPart1, memory)
-for multiplication in uncorruptMemory:
-    part1answer += (int(multiplication[1]) * int(multiplication[2]))
+print(f'The answer to day 1 part 1 = {part1(input)}')
 
 # Part 2
-uncorruptMemory = []
-patternPart2 = r"don't\(\)(.*?)do\(\)"
-donotMemory = re.findall(patternPart2, memory, re.DOTALL)
-for memory in donotMemory:
-    print(memory)
-    uncorruptMemory = re.findall(patternPart1, memory)
-    for multiplication in uncorruptMemory:
-        print(multiplication)
-        part2answer += (int(multiplication[1]) * int(multiplication[2]))
-
-part2answer = part1answer - part2answer
-
-print(f'The answer to day 1 part 1 = {part1answer}')
-print(f'The answer to day 1 part 2 = {part2answer}')
+print(f'The answer to day 1 part 2 = {part2(input, part1(input))}')
--- Day X/DayX.py
+++ Day X/DayX.py
@@ -1,8 +1,12 @@
-# variables
-part1answer = 0
-part2answer = 0
+## variables ##
+filePaths = {
+    '1': 'Day X\Input files\Input.txt',
+    '2': 'Day X\Input files\TestInput.txt',
+    '3': 'Day X\Input files\TestInputPart2.txt'
+}
 
-def parse_file(filepath):
+## Methods ##
+def parseFile(filepath):
     parsedFile = ""
     with open(filepath, 'r') as file:
         for line in file:
@@ -10,14 +14,23 @@ def parse_file(filepath):
     return parsedFile
 
 def part1(input):
+    part1answer = 0
     return part1answer
 
 def part2(input):
+    part2answer = 0
     return part1answer
 
-# input = parse_file('Day X\Input files\Input.txt')
-input = parse_file('Day X\Input files\TestInput.txt')
-# input = parse_file('Day X\Input files\TestInputPart2.txt')
+## Main execution ##
+# Prompt user for input choice and parse file
+print("""
+Select input file to use:
+    1. Main Input
+    2. Test Input
+    3. Test Input part 2
+""")
+choice = input("Enter choice (1/2/3): ")
+input = parseFile(filePaths[choice])
 
 # Part 1
 print(f'The answer to day 1 part 1 = {part1(input)}')
