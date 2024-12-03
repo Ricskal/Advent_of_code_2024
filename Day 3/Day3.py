501bc3808367251f55011d726432132bf0b3247b
Rick <rick.ensink@ns.nl>
2024-12-03 11:39:08 +0100

    day 3 part 1 en 2

--- Day 3/Day3.py
+++ Day 3/Day3.py
@@ -1,21 +1,37 @@
+import re
+
 # variables
-file = open('Day X\Input files\InputDayX.txt', 'r')
-line_list = file.readlines()
-list1 = []
-list2 = []
+file = open('Day 3\Input files\Input.txt', 'r')
+# file = open('Day 3\Input files\TestInput.txt', 'r')
+# file = open('Day 3\Input files\TestInputPart2.txt', 'r')
+lineList = file.readlines()
+memory = ''
+part1answer = 0
+part2answer = 0
 
 # Process file
-for line in line_list:
-    list1.append(int(line.split()[0]))
-    list2.append(int(line.split()[1]))
-list1.sort()
-list2.sort()
+for line in lineList:
+    memory += line
 
 # Part 1
-part1answer = 0
-# print(f'The answer to day 1 part 1 = {part1answer}')
-
+uncorruptMemory = []
+patternPart1 = r"(mul)\((\d{1,3}),(\d{1,3})\)"
+uncorruptMemory = re.findall(patternPart1, memory)
+for multiplication in uncorruptMemory:
+    part1answer += (int(multiplication[1]) * int(multiplication[2]))
 
 # Part 2
-part2answer = 0
-# print(f'The answer to day 1 part 2 = {part2answer}')
+uncorruptMemory = []
+patternPart2 = r"don't\(\)(.*?)do\(\)"
+donotMemory = re.findall(patternPart2, memory, re.DOTALL)
+for memory in donotMemory:
+    print(memory)
+    uncorruptMemory = re.findall(patternPart1, memory)
+    for multiplication in uncorruptMemory:
+        print(multiplication)
+        part2answer += (int(multiplication[1]) * int(multiplication[2]))
+
+part2answer = part1answer - part2answer
+
+print(f'The answer to day 1 part 1 = {part1answer}')
+print(f'The answer to day 1 part 2 = {part2answer}')
