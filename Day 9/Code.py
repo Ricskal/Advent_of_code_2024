e2ee7adc05e36c14a913434ba1796f8ac5002f64
Rick <rick.ensink@ns.nl>
2024-12-09 21:37:28 +0100

    save

--- Day 9/Code.py
+++ Day 9/Code.py
@@ -17,17 +17,37 @@ expectedTestOutputPart2 = 0
 
 ## Methods ##
 def parse_file(filepath):
-    parsedFile = ""
+    diskMap = ""
     with open(filepath, 'r') as file:
         for line in file:
-            parsedFile += line
-    print(parsedFile)
-    return parsedFile
+            diskMap += line
+    print(f'diskMap = {diskMap}')
+    return diskMap
 
 # 2333133121414131402
 # 00...111...2...333.44.5555.6666.777.888899
-def convert_disk_map(parsedFile):
-    
+def convert_disk_map(diskMap):
+    isFile = True
+    blockList = []
+    fileID = -1
+    for command in diskMap:
+        command = int(command)
+        if isFile:
+            fileID += 1
+            fileBlocks = command
+            for i in range(fileBlocks):
+                blockList.append(fileID)
+            isFile = False
+        else:
+            emptyBlocks = command
+            for i in range(emptyBlocks):
+                blockList.append('.')
+            isFile = True
+    print(f'blockList = {blockList}')
+    return blockList
+
+def defragment_disk(blockList):
+    return True
 
 def part_1(input):
     part1answer = 0
@@ -49,11 +69,13 @@ else:
     choice = input("Enter choice (1/2): ")
     
 # Parse the input file and process it into data
-input = parse_file(filePaths[choice])
+diskMap = parse_file(filePaths[choice])
+blockList = convert_disk_map(diskMap)
+defragment_disk(blockList)
 
 # Output results for both parts and verify test results if applicable
 # Part 1 outputs
-part1answer = part_1(input)
+part1answer = part_1(diskMap)
 print(f'The answer to day {day} part 1 = {part1answer}')
 if choice == '2':
     testCorrect = part1answer == expectedTestOutputPart1
