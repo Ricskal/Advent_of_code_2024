2f916f4369193771dd2f20b1b60ee5952335417f
Rick <rick.ensink@ns.nl>
2024-12-12 17:10:26 +0100

    save

--- Day 10/Code.py
+++ Day 10/Code.py
@@ -12,19 +12,57 @@ filePaths = {
 }
 # Default configuration for input file and expected outputs for tests
 defaultFile = True
-expectedTestOutputPart1 = 36
+expectedTestOutputPart1 = 2
 expectedTestOutputPart2 = 0
 
 ## Methods ##
 def parse_file(filepath):
-    parsedFile = ""
+    topographicTileList = []
     with open(filepath, 'r') as file:
         for line in file:
-            parsedFile += line
-    return parsedFile
+            topographicTileList.append(line.strip())
+    return topographicTileList
 
-def part_1(input):
+def map_topographic_tiles(topographicMapList):
+    # Map topographic map positions to coordinates and organizes them into a dictionary
+    # (xCord , yCord) = tile
+    topographicTileDict = {}
+    trailheadList = []
+    yCord = -1
+    for topographicMap in topographicMapList:
+        yCord += 1
+        xCord = -1
+        for tile in topographicMap:
+            xCord += 1
+            if tile.isdigit(): tile = int(tile)
+            topographicTileDict[(xCord, yCord)] = tile
+            if tile == 0: trailheadList.append((xCord, yCord))
+    maxBound = (xCord, yCord)
+    print(f'trailheadList = {trailheadList}')
+    return topographicTileDict, trailheadList, maxBound
+
+possibleTileDict = {}
+def get_possible_tiles(topographicTileDict, coordinate):
+    possibleTileList = []
+    currentTileValue = topographicTileDict[coordinate]
+    serroundingTileList = [
+        (coordinate[0], coordinate[1] -1) # North
+        ,(coordinate[0] +1, coordinate[1]) # East
+        ,(coordinate[0], coordinate[1] +1) # South
+        ,(coordinate[0] -1, coordinate[1]) # West
+    ]
+    for serroundingTile in serroundingTileList:
+        serroundingTileValue = topographicTileDict.get(serroundingTile, currentTileValue)
+        if serroundingTileValue - currentTileValue == 1:
+            possibleTileList.append(serroundingTile)
+    if possibleTileList: possibleTileDict[coordinate] = possibleTileList
+    return possibleTileDict
+
+def part_1(topographicTileDict, trailheadList, maxBound):
     part1answer = 0
+    for coordinate in topographicTileDict:
+        possibleTileDict = get_possible_tiles(topographicTileDict, coordinate)
+    print(possibleTileDict)
     return part1answer
 
 def part_2(input):
@@ -43,11 +81,12 @@ else:
     choice = input("Enter choice (1/2): ")
     
 # Parse the input file and process it into data
-input = parse_file(filePaths[choice])
+topographicTileList = parse_file(filePaths[choice])
+topographicTileDict, trailheadList, maxBound = map_topographic_tiles(topographicTileList)
+part1answer = part_1(topographicTileDict, trailheadList, maxBound)
 
 # Output results for both parts and verify test results if applicable
 # Part 1 outputs
-part1answer = part_1(input)
 print(f'The answer to day {day} part 1 = {part1answer}')
 if choice == '2':
     testCorrect = part1answer == expectedTestOutputPart1
