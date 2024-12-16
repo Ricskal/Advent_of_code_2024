b5d663a5a3ead744149613aae841ca3e9fec4047
Rick <rick.ensink@ns.nl>
2024-12-16 21:49:24 +0100

    save

--- Day 12/Code.py
+++ Day 12/Code.py
@@ -17,14 +17,68 @@ expectedTestOutputPart2 = 0
 
 ## Methods ##
 def parse_file(filepath):
-    parsedFile = ""
+    _list0 = []
+    flowerfieldDict, flowerCountDict = {}, {}
     with open(filepath, 'r') as file:
         for line in file:
-            parsedFile += line
-    return parsedFile
+            _list0.append(line.strip())
+    
+    yCord = -1
+    for _line in _list0:
+        yCord += 1
+        xCord = -1
+        for _tile in _line:
+            xCord += 1
+            flowerfieldDict[(xCord, yCord)] = _tile
+    
+    for flower in flowerfieldDict.values():
+        flowerCountDict[flower] = flowerCountDict.get(flower, 0) + 1
+        
+    return flowerfieldDict, flowerCountDict
 
-def part_1(input):
+def part_1(flowerfieldDict, flowerCountDict):
     part1answer = 0
+    flowerSumDict = {}
+    directionDict = {
+        'North': (0, -1),
+        'East': (1, 0), 
+        'South': (0, 1),
+        'West': (-1, 0)
+    }
+    for flowerCor in flowerfieldDict:
+        
+        flower = flowerfieldDict[flowerCor]
+        notSameFlowerCount = flowerfieldDict.get(flowerCor[1], 0)
+        newFlower = flowerfieldDict.get((flowerCor[0] + directionDict['North'][0], flowerCor[1] + directionDict['North'][1]), '&')[0]
+        if newFlower != flower:
+            notSameFlowerCount += 1
+            flowerfieldDict[flowerCor] = [flower, notSameFlowerCount]
+            
+        newFlower = flowerfieldDict.get((flowerCor[0] + directionDict['East'][0], flowerCor[1] + directionDict['East'][1]), '&')[0]
+        if newFlower != flower:
+            notSameFlowerCount += 1
+            flowerfieldDict[flowerCor] = [flower, notSameFlowerCount]
+
+        newFlower = flowerfieldDict.get((flowerCor[0] + directionDict['South'][0], flowerCor[1] + directionDict['South'][1]), '&')[0]
+        if newFlower != flower:
+            notSameFlowerCount += 1
+            flowerfieldDict[flowerCor] = [flower, notSameFlowerCount]
+
+        newFlower = flowerfieldDict.get((flowerCor[0] + directionDict['West'][0], flowerCor[1] + directionDict['West'][1]), '&')[0]
+        if newFlower != flower:
+            notSameFlowerCount += 1
+            flowerfieldDict[flowerCor] = [flower, notSameFlowerCount]
+        
+        if not isinstance(flowerfieldDict[flowerCor], list): 
+            flowerfieldDict[flowerCor] = [flower, 0]
+
+    for flower in flowerfieldDict.values():
+        flowerSumDict[flower[0]] = flowerSumDict.get(flower[0], 0) + flower[1]
+        # flowerSumDict[flower[0]] = flowerSumDict.get(flower[0], 0) + flowerfieldDict[flower[1]]
+        
+    
+    print(flowerSumDict)
+    
     return part1answer
 
 def part_2(input):
@@ -43,18 +97,18 @@ else:
     choice = input("Enter choice (1/2): ")
     
 # Parse the input file and process it into data
-input = parse_file(filePaths[choice])
+flowerfieldDict, flowerCountDict = parse_file(filePaths[choice])
+part1answer = part_1(flowerfieldDict, flowerCountDict)
+part2answer = part_2(input)
 
 # Output results for both parts and verify test results if applicable
 # Part 1 outputs
-part1answer = part_1(input)
 print(f'The answer to day {day} part 1 = {part1answer}')
 if choice == '2':
     testCorrect = part1answer == expectedTestOutputPart1
     print(f'This answer is {testCorrect}! Expected {expectedTestOutputPart1} and got {part1answer}')
 
 # Part 2 outputs
-part2answer = part_2(input)
 print(f'The answer to day {day} part 2 = {part2answer}')
 if choice == '2':
     testCorrect = part2answer == expectedTestOutputPart2
